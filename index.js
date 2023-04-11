83db3ce0cd1b769920266c4b3180cd5fba8e6a68
Fadia Bortol <fadiabortol6@gmail.com>
2023-04-11 15:45:21 +0300

    adding new EndPoint

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/',(req,res)=>{
 res.send('Hi All');
 })
 
+app.get('/data',(req,res)=>{
+    var data = ['Ali beik','Georgio','Ahmad','Rym']
+    res.send(data);
+    })
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
